4520c7598a613c4512647ba2f86a8dc0a9a2af9e
Louis Maddox <louismmx@gmail.com>
2023-10-30 09:52:50 +0000

    refactor(cli): split off validation handling; no negated flags in usage hint

--- src/classipypi/cli.py
+++ src/classipypi/cli.py
@@ -10,18 +10,24 @@ from .select import select_tags
 
 __all__ = ["run_cli"]
 
+commands = {"sel": SelectorConfig, "ls": ListingConfig}
+
+
+def handle_validation_error(ve: ValidationError) -> str:
+    chosen_command = {v.__name__: k for k, v in commands.items()}[ve.title]
+    error_msgs = "\n".join(str(e["ctx"]["error"]) for e in ve.errors())
+    msg = f"Invalid {chosen_command!r} command:\n" + indent(error_msgs, prefix="- ")
+    print(msg, end="\n\n", file=stderr)
+    return chosen_command
+
 
 def run_cli():
-    commands = {"sel": SelectorConfig, "ls": ListingConfig}
     try:
         config = defopt.run(commands, no_negated_flags=True)
     except ValidationError as ve:
-        error_msgs = "\n".join(str(e["ctx"]["error"]) for e in ve.errors())
-        print("The command you ran was invalid for the following reason:", file=stderr)
-        print(indent(error_msgs, prefix="- "), end="\n\n", file=stderr)
-        chosen_command = {v.__name__: k for k, v in commands.items()}[ve.title]
+        chosen_command = handle_validation_error(ve)
         try:
-            defopt.run(commands[chosen_command], argv=["-h"])
+            defopt.run(commands[chosen_command], argv=["-h"], no_negated_flags=True)
         except SystemExit as exc:
             exc.code = 1
             raise
